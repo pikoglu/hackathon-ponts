0c2b5a59fe5e30f01203203b1e5e2f86619ef8fc
Félix Fourreau <felix.fourreau@eleves.enpc.fr>
2023-09-07 10:01:04 +0200

    vrai ou faux

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def prompt():
 
 @app.route("/question", methods=['GET'])
 def question():
-    question = "Genere une question pertinente sur le texte"
+    question = "Genere en francais une seule question pertinente sur le texte"
     user_answer = ask_question_to_pdf(question)
     print(user_answer)
     return {'answer':user_answer}
@@ -29,6 +29,6 @@ def answer():
     return {'answer':reponse}
 
 def is_answer_correct(question, user_answer):
-    evaluation_prompt = f"Question: {question}\n Answer: {user_answer}\n La réponse est-elle correcte ?\n Answer:"
+    evaluation_prompt = f"La question portant sur le texte était : {question}\n La réponse apportée : {user_answer}\n Si la réponse n'est pas correcte dis moi FAUX et donne moi une explication concise de la réponse correcte. \n Si la réponse est correcte dis moi VRAI et felicite moi "
     response=ask_question_to_pdf(evaluation_prompt)
     return response
